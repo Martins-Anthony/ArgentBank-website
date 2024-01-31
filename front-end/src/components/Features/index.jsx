3a8df9e7539557cd2cac5222986bd47764534c96
Martins-Anthony <thony.martins.67@gmail.com>
2024-01-31 14:30:26 +0100

    components Feature

---
+++ front-end/src/components/Features/index.jsx
@@ -0,0 +1,12 @@
+
+function Feature({cover, alt, title, text}) {
+  return (
+    <div className="feature-item">
+      <img src={cover} alt={alt} className="feature-icon" />
+      <h3 className="feature-item-title">{title}</h3>
+      <p>{text}</p>
+    </div>
+  )
+}
+
+export default Feature
--- front-end/src/pages/Home/index.jsx
+++ front-end/src/pages/Home/index.jsx
@@ -1,3 +1,8 @@
+import Feature from "../../components/Features"
+import iconChat from "../../assets/icon-chat.png"
+import iconMoney from "../../assets/icon-money.png"
+import iconSecurity from "../../assets/icon-security.png"
+
 function Home() {
   return (
     <main>
@@ -12,37 +17,26 @@ function Home() {
       </div>
       <section className="features">
         <h2 className="sr-only">Features</h2>
-        <div className="feature-item">
-          <img src="./img/icon-chat.png" alt="Chat Icon" className="feature-icon" />
-          <h3 className="feature-item-title">You are our #1 priority</h3>
-          <p>
-            Need to talk to a representative? You can get in touch through our
-            24/7 chat or through a phone call in less than 5 minutes.
-          </p>
-        </div>
-        <div className="feature-item">
-          <img
-            src="./img/icon-money.png"
-            alt="Chat Icon"
-            className="feature-icon"
-          />
-          <h3 className="feature-item-title">More savings means higher rates</h3>
-          <p>
-            The more you save with us, the higher your interest rate will be!
-          </p>
-        </div>
-        <div className="feature-item">
-          <img
-            src="./img/icon-security.png"
-            alt="Chat Icon"
-            className="feature-icon"
-          />
-          <h3 className="feature-item-title">Security you can trust</h3>
-          <p>
-            We use top of the line encryption to make sure your data and money
-            is always safe.
-          </p>
-        </div>
+        <Feature 
+          cover={iconChat} 
+          alt={"Chat Icon"} 
+          title={"You are our #1 priority"} 
+          text={`Need to talk to a representative? You can get in touch through our
+            24/7 chat or through a phone call in less than 5 minutes.`} 
+        />
+        <Feature 
+          cover={iconMoney} 
+          alt={"Chat Icon"} 
+          title={"More savings means higher rates"} 
+          text={`The more you save with us, the higher your interest rate will be!`} 
+        />
+        <Feature 
+          cover={iconSecurity} 
+          alt={"Chat Icon"} 
+          title={"Security you can trust"} 
+          text={`We use top of the line encryption to make sure your data and money
+          is always safe.`} 
+        />
       </section>
     </main>
   )
