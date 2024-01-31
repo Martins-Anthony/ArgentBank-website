0769f0e0bba25fe6e24ab9aa1a1771334b36d8ca
Martins-Anthony <thony.martins.67@gmail.com>
2024-01-31 15:01:01 +0100

    component Logo

---
+++ front-end/src/components/layout/Logo/index.jsx
@@ -0,0 +1,13 @@
+import argentBankLogo from "../../../assets/argentBankLogo.png"
+
+function Logo() {
+  return (
+    <img
+      className="main-nav-logo-image"
+      src={ argentBankLogo }
+      alt="Argent Bank Logo"
+    />  
+  )
+}
+
+export default Logo
--- front-end/src/components/layout/NavBar/index.jsx
+++ front-end/src/components/layout/NavBar/index.jsx
@@ -1,20 +1,17 @@
-import argentBankLogo from "../../../assets/argentBankLogo.png"
 import { Link } from "react-router-dom"
+import Logo from "../Logo"
 
 function NavBar() {
   return (
     <nav className="main-nav">
       <Link to={'/'} className="main-nav-logo" >
-        <img
-          className="main-nav-logo-image"
-          src={ argentBankLogo }
-          alt="Argent Bank Logo"
-        />
+      <Logo /> 
         <h1 className="sr-only">Argent Bank</h1>
       </Link>
-      <div>
+      <div >
         <Link className="main-nav-item" to={"/sign-in"}>
           <i className="fa fa-user-circle"></i>
+          {" "}
           Sign In
         </Link>
       </div>
