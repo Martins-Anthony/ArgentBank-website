5f0482ecb7aac65f27a1219f2f5c5fb268d073e6
Martins <thony.martins.67@gmail.com>
2024-02-11 12:11:58 +0100

    adding condition Route "/user"

--- front-end/src/components/App/AppRouter/index.jsx
+++ front-end/src/components/App/AppRouter/index.jsx
@@ -6,15 +6,18 @@ import Footer from '../../layout/Footer'
 import SignIn from '../../../pages/SignIn'
 import Users from '../../../pages/Users'
 import Error from '../../layout/Error'
+import { selectUser } from '../selectors'
+import { useSelector } from 'react-redux'
 
 function AppRouter() {
+  const user = useSelector(selectUser)
   return (
     <Router>
       <NavBar />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/user" element={<Users />} />
+        {user ? <Route path="/user" element={<Users />} /> : null}
         <Route path="*" element={<Error />} />
       </Routes>
       <Footer />
